576f8f0ff4a8a98a2846185f72bfa908cf55bc3e
Jeremy Heminger ᓚᘏᗢ <contact@jeremyheminger.com>
2025-10-07 14:11:02 -0700

    Update v_aq_bom_lookup.sql version 1.0.0.1

--- v_aq_bom_lookup.sql
+++ v_aq_bom_lookup.sql
@@ -2,20 +2,25 @@
  * @title v_aq_bom_lookup
  * @about parent child standard_id and arinvt_id and partno_id
  * @author Jeremy Heminger
- * @date October 1, 2025
- * @last_update October 1, 2025
+ * @date october 1, 2025
+ * @last_update october 7, 2025
  * 
- * @version 1.0.0.0
+ * @version 1.0.0.1
  *
  * @live false
  * */
 create or replace view v_aq_bom_lookup as
 	select 
     	distinct 
-    s.id as standard_id,a.id as arinvt_id,bd.partno_id
-from 
-    standard s,partno p,bom_depend bd,sndop sd,opmat op,arinvt a 
-    where p.standard_id = s.id 
+            s.id as standard_id,a.id as arinvt_id,bd.partno_id,sd.id as sndop_id,p.arinvt_id as parent_arinvt_id
+	from 
+	    standard s,
+	    partno p,
+	    bom_depend bd,
+	    sndop sd,
+	    opmat op,
+	    arinvt a 
+	where p.standard_id = s.id 
 	    and p.id = bd.partno_id 
 	    and bd.sndop_id = sd.id 
 	    and sd.id = op.sndop_id
